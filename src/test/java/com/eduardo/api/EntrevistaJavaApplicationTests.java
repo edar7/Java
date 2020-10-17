3f3f84a493e0311bcd6a326fbbca6a6c4f2aaf5c
eduardo.araujo7 <eduardo.araujo7@gmail.com>
2020-10-17 18:46:18 -0300

    versão inicial do Projeto Entrevista

---
+++ src/test/java/com/eduardo/api/EntrevistaJavaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eduardo.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class EntrevistaJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
