555da7f340ef2dcbd0c469acdd785a7eee63630b
eduardo.araujo7 <eduardo.araujo7@gmail.com>
2020-10-17 22:20:54 -0300

    integracao com travisCI

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/edar7/Java.svg?branch=main)](https://travis-ci.org/edar7/Java)
 # Java
 Java Spring Boot API REST
--- src/main/java/com/eduardo/api/EntrevistaJavaApplication.java
+++ src/main/java/com/eduardo/api/EntrevistaJavaApplication.java
@@ -8,7 +8,7 @@ public class EntrevistaJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntrevistaJavaApplication.class, args);
-		System.out.println("OK");
+		
 	}
 
 }
